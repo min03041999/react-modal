ab5fb0d9baf4a6c7e35d6a75deb5fb826cc5e384
DESKTOP-4GUMU4J\Amin <min03041999@gmail.com>
2022-04-17 21:07:49 +0700

    feat(source): add source

---
+++ src/components/button/Button.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./Button";
+
+function Button(props) {
+  return <div>Button</div>;
+}
+
+export default Button;
---
+++ src/components/modal/Modal.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./modal.scss";
+
+function Modal(props) {
+  return <div>Modal</div>;
+}
+
+export default Modal;
