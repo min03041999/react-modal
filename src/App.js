8162afe986293bfee9fac6394c590409ad482a51
DESKTOP-4GUMU4J\Amin <min03041999@gmail.com>
2022-04-18 17:36:27 +0700

    feat(modal): add modal

--- src/App.js
+++ src/App.js
@@ -1,7 +1,40 @@
+import { useState } from "react";
 import "./App.scss";
+import Button from "./components/button/Button";
+import Modal, {
+  ModalBody,
+  ModalFooter,
+  ModalHeader,
+} from "./components/modal/Modal";
 
 function App() {
-  return <div></div>;
+  const [showModal, setShowModal] = useState(false);
+
+  return (
+    <div>
+      <Button onClick={() => setShowModal(true)}>Open modal</Button>
+      <Modal
+        show={showModal}
+        setShow={setShowModal}
+        // hideCloseButton
+      >
+        <ModalHeader>
+          <h2>Modal header</h2>
+        </ModalHeader>
+        <ModalBody>
+          <p style={{ textAlign: "justify" }}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt
+            maxime dolorem asperiores laboriosam ad delectus ea. Tempora tempore
+            repellendus laudantium fugiat saepe mollitia eius illo possimus
+            laborum consequuntur, tenetur neque.
+          </p>
+        </ModalBody>
+        <ModalFooter>
+          <Button onClick={() => setShowModal(false)}>Close</Button>
+        </ModalFooter>
+      </Modal>
+    </div>
+  );
 }
 
 export default App;
